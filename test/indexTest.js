45a623b5a4d58d609c01893b16bb938134f94d12
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-07-06 15:58:21 -0400

    Adds tests

---
+++ test/indexTest.js
@@ -0,0 +1,9 @@
+const expect = chai.expect;
+
+describe('index.js', function () {
+  describe('moveDodgerRight()', function () {
+    it('Checks for a moveDodgerRight() function', function () {
+      expect(moveDodgerRight()).to.equal(moveDodgerRight());
+    });
+  });
+});
