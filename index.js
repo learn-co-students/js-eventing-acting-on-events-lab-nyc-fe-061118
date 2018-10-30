d2d612c6349883acff3c43aff59930bf604127e6
Steven G. Harms <steven.harms@flatironschool.com>
2018-10-30 15:22:38 -0400

    solution

---
+++ index.js
@@ -0,0 +1,16 @@
+dodger.style.backgroundColor = '#ff5bab';
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+})
